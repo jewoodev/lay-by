ed7c578ea08a2d0b8a7d51103267ab756f107062
jewoodev <jewoos15@naver.com>
2024-04-25 01:44:40 +0900

    📰 feat(msa): api-gateway service

---
+++ api-gateway-service/src/main/java/com/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,15 @@
+package com.apigatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class FilterConfig {
+
+    Environment env;
+
+    public FilterConfig(Environment env) {
+        this.env = env;
+    }
+}
