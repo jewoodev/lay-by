c115e84560abe2664c9a6b06d8c9e52903e97fcc
jewoodev <jewoos15@naver.com>
2024-04-25 01:45:37 +0900

    📰 feat(msa): auth service

---
+++ auth-service/src/main/java/com/authservice/web/exception/CertificationFailedException.java
@@ -0,0 +1,19 @@
+package com.authservice.web.exception;
+
+public class CertificationFailedException extends RuntimeException {
+    public CertificationFailedException() {
+        super();
+    }
+
+    public CertificationFailedException(String message) {
+        super(message);
+    }
+
+    public CertificationFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CertificationFailedException(Throwable cause) {
+        super(cause);
+    }
+}
