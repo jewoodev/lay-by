565bd7e0679293e3cc75b07cc695ea43031e15da
jewoodev <jewoos15@naver.com>
2024-05-04 01:47:33 +0900

    📰 refactor(delivery): solve the issue #22

---
+++ order-service/src/main/java/com/orderservice/web/controller/DeliveryController.java
@@ -0,0 +1,25 @@
+package com.orderservice.web.controller;
+
+import com.orderservice.web.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/delivery")
+public class DeliveryController {
+
+    private final DeliveryService deliveryService;
+
+    @PostMapping("/{address_id}")
+    public Long createDelivery(
+            @PathVariable(name = "address_id") Long addressId
+    ) {
+        return deliveryService.createDelivery(addressId);
+    }
+}
